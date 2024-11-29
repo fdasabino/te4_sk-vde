f0a3137804fd683acac91022a00ce5f0a526bbfe
Francisco Sabino <fdasabino@gmail.com>
2024-11-29 12:42:28 +0100

    Refactor application structure: remove unused files and update styling

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to React Hooks</h1>
+      <ul>
+        <li>Introduction</li>
+        <li>useState</li>
+        <li>useEffect</li>
+        <li>useContext</li>
+        <li>useReducer</li>
+        <li>useRef</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
